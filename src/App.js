1245aebee12cab1f13d1acc1d96f9ae8288b5ee2
ayu-tw <80678206+ayu-tw@users.noreply.github.com>
2021-11-23 11:55:07 +0100

    added header, footer and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather application</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by
+          <a href="#" target="_blank">
+            AnYu Huang
+          </a>
+          and is{" "}
+          <a href="#" target="_blank">
+            open-resourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
